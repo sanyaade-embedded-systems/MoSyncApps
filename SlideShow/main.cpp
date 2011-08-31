b49054486dc3c15fc1d35b7abb4a5006fa546734
Mikael Kindborg <mikael.kindborg@mosync.com>
2011-08-31 10:52:32 +0200

    Added file header comment which provides some documentation and info about the example.

--- SlideShow/main.cpp
+++ SlideShow/main.cpp
@@ -1,6 +1,70 @@
+/*
+ * Copyright (c) 2011 MoSync AB
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
-
-// TODO: Add file header & comment.
+/**
+ * @file main.cpp
+ * @author Mikael Kindborg
+ *
+ * Application that displays a slide show. Touch right half of the
+ * screen to go to the next slide, touch left half to go to the previous
+ * slide.
+ *
+ * To keep this example simple, no scaling of the images is currently done.
+ *
+ * This example shows how to use a simple caching mechanism to load
+ * and unload images, in order to prevent out-of-memory problems.
+ *
+ * On Android, the bitmap memory that can be used by an application
+ * is limited. If you have too many/too big bitmaps you get the error:
+ *
+ *   java.lang.OutOfMemoryError: bitmap size exceeds VM budget
+ *
+ * Note that there are 24 images in the slide show, but only 6 unique
+ * images. The images will be shown repeatedly. The reuse of the images
+ * is just a way of stressing the memory limit (24 unloaded binary
+ * resources will be allocated).
+ *
+ * If you inspect the log (adb logcat on Android), you can see how
+ * images get loaded and unloaded as you move through the slides.
+ *
+ * The .ubin resource type does not load the resource into memory when
+ * the application starts, but keeps them "on disk" in an unloaded state.
+ * Whenever there is a syscall that takes a handle to a ubin resource
+ * as a parameter, the data is read from disk and an object is created
+ * in memory using that data (like an image). You then call maDestoryObject
+ * to free the allocated object (for example an image). The resource is
+ * still on disk, and therefore if can again be used to create a new object.
+ * This is the way the caching mechanism used in this program works.
+ *
+ * Resource ids (handle values) start at 1, and in this program we
+ * never refer to resource by name, but by the sequence number (index).
+ * If you want to add other resources to the program, you can add them at
+ * the end of the resource file, and refer to them by name. In that case
+ * you need to include the resource definitions like this:
+ *
+ *   #include "MAHeaders.h"
+ *
+ * Happy hacking! :)
+ */
 
 #include <MAUtil/Moblet.h>
 #include <conprint.h>
@@ -11,8 +75,7 @@ using namespace MAUtil;
 #define NUMBER_OF_IMAGES 24
 
 /**
- * Moblet that displays images (slides). To keep this
- * example simple, no scaling of the images is done.
+ * Moblet that displays images (slides).
  */
 class SlideShowMoblet : public Moblet
 {
@@ -92,6 +155,7 @@ public:
 		else
 		{
 			// Display a red screen if there is no image.
+			// (Might happen in low memory conditions.)
 			maSetColor(0xFF0000);
 			maFillRect(0, 0, 10000, 10000);
 			maUpdateScreen();
@@ -167,8 +231,24 @@ public:
 	}
 
 private:
+	/**
+	 * The total number of images.
+	 */
 	int mNumberOfImages;
+
+	/**
+	 * The index of the image shown (current slide).
+	 */
 	int mCurrentImageIndex;
+
+	/**
+	 * Array that hold handles to images.
+	 *
+	 * TODO: It would be generally more useful to have
+	 * a vector for this, since the list can then easily
+	 * grow, if you download images and add to the slideshow,
+	 * for example.
+	 */
 	MAHandle mImageCache[NUMBER_OF_IMAGES];
 };
 
